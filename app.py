e2f1ebbbd10714686d2c2dfb1fd45284e187595b
Tu Nombreavril <avril@teams.com.mx>
2025-09-22 21:16:02 -0600

    ji

--- app.py
+++ app.py
@@ -569,7 +569,7 @@ def index_page():
     # >>> Reto Foto (activo por photo_challenges o por flag legado 'retos')
     foto_activo = get_active_photo_challenge()
     if not foto_activo:
-        foto_activo = ensure_default_photo_challenge_from_legacy_flag()
+      #  foto_activo = ensure_default_photo_challenge_from_legacy_flag()
 
     mi_foto = None
     if foto_activo:
